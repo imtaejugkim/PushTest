78a96cb893f33d0734907a58d074b181bd304c36
TaejungKim <tae0803@naver.com>
2025-01-06 20:22:11 +0900

    chore: typography 추가

--- app/src/main/java/com/example/pushtest/ui/theme/Type.kt
+++ app/src/main/java/com/example/pushtest/ui/theme/Type.kt
@@ -1,34 +1,131 @@
 package com.example.pushtest.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.pushtest.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+val suiteBold = FontFamily(Font(R.font.suite_bold))
+val suiteRegular = FontFamily(Font(R.font.suite_regular))
+
+@Immutable
+data class PushTestTypography(
+    // title
+    val title_b_24: TextStyle,
+    val title_b_22: TextStyle,
+    val title_r_24: TextStyle,
+    val title_r_22: TextStyle,
+
+
+    // body
+    val body_b_20: TextStyle,
+    val body_b_18: TextStyle,
+    val body_b_16: TextStyle,
+    val body_b_14: TextStyle,
+    val body_r_20: TextStyle,
+    val body_r_18: TextStyle,
+    val body_r_16: TextStyle,
+    val body_r_14: TextStyle,
+
+    // detail
+    val detail_b_12: TextStyle,
+    val detail_r_12: TextStyle
 )
+
+val defaultPushTestTypography = PushTestTypography(
+    // title
+    title_b_24 = TextStyle(
+        fontFamily = suiteBold,
+        fontSize = 24.sp,
+        fontWeight = FontWeight(700),
+        lineHeight = 36.sp
+    ),
+    title_b_22 = TextStyle(
+        fontFamily = suiteBold,
+        fontSize = 22.sp,
+        fontWeight = FontWeight(400),
+        lineHeight = 33.sp
+    ),
+    title_r_24 = TextStyle(
+        fontFamily = suiteRegular,
+        fontSize = 24.sp,
+        fontWeight = FontWeight(700),
+        lineHeight = 36.sp
+    ),
+    title_r_22 = TextStyle(
+        fontFamily = suiteRegular,
+        fontSize = 22.sp,
+        fontWeight = FontWeight(400),
+        lineHeight = 33.sp
+    ),
+
+    //body
+    body_b_20 = TextStyle(
+        fontFamily = suiteBold,
+        fontSize = 20.sp,
+        fontWeight = FontWeight(700),
+        lineHeight = 30.sp
+    ),
+    body_b_18 = TextStyle(
+        fontFamily = suiteBold,
+        fontSize = 18.sp,
+        fontWeight = FontWeight(700),
+        lineHeight = 27.sp
+    ),
+    body_b_16 = TextStyle(
+        fontFamily = suiteBold,
+        fontSize = 16.sp,
+        fontWeight = FontWeight(700),
+        lineHeight = 24.sp
+    ),
+    body_b_14 = TextStyle(
+        fontFamily = suiteBold,
+        fontSize = 14.sp,
+        fontWeight = FontWeight(700),
+        lineHeight = 20.sp
+    ),
+    body_r_20 = TextStyle(
+        fontFamily = suiteRegular,
+        fontSize = 20.sp,
+        fontWeight = FontWeight(400),
+        lineHeight = 30.sp
+    ),
+    body_r_18 = TextStyle(
+        fontFamily = suiteRegular,
+        fontSize = 18.sp,
+        fontWeight = FontWeight(400),
+        lineHeight = 27.sp
+    ),
+    body_r_16 = TextStyle(
+        fontFamily = suiteRegular,
+        fontSize = 16.sp,
+        fontWeight = FontWeight(400),
+        lineHeight = 24.sp
+    ),
+    body_r_14 = TextStyle(
+        fontFamily = suiteRegular,
+        fontSize = 14.sp,
+        fontWeight = FontWeight(400),
+        lineHeight = 20.sp
+    ),
+
+    // detail
+    detail_b_12 = TextStyle(
+        fontFamily = suiteBold,
+        fontSize = 12.sp,
+        fontWeight = FontWeight(700),
+        lineHeight = 17.sp
+    ),
+    detail_r_12 = TextStyle(
+        fontFamily = suiteRegular,
+        fontSize = 12.sp,
+        fontWeight = FontWeight(400),
+        lineHeight = 17.sp
+    )
+)
+
+val LocalTypo = staticCompositionLocalOf { defaultPushTestTypography }
